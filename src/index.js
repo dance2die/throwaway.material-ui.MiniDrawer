652b5a5449859d96d4ec25d08640fdb8de405319
Sung Kim <sungmkim80@gmail.com>
2018-07-21 00:30:49 -0400

    added CSS base line
    because of scrollbars.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+
 import MiniDrawer from "./MiniDrawer";
 import tileData from "./tileData";
 
 const App = () => (
   <div>
+    <CssBaseline />
     <MiniDrawer />
   </div>
 );
